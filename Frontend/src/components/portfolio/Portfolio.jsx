77e1fa13d6de8a06e38851154728b60d4c8c44dd
kumar-Quicktouch <kumar.sanu@quicktouch.co.in>
2025-05-04 20:47:48 +0530

    chnagesa added

--- Frontend/src/components/portfolio/Portfolio.jsx
+++ Frontend/src/components/portfolio/Portfolio.jsx
@@ -1,31 +1,36 @@
-import useQuery from '@/hooks/useQuery'
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { PUBLIC_PORTFOLIO } from '@/imports/api'
-import { Loader2 } from 'lucide-react'
-import { MinimalistTemplate, MinimalTemplate, ModernTemplate } from '../home/preview/previewTemplate/PreviewTemplate'
+import useQuery from "@/hooks/useQuery";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { PUBLIC_PORTFOLIO } from "@/imports/api";
+import { Loader2 } from "lucide-react";
+import {
+  MinimalistTemplate,
+  MinimalTemplate,
+  ModernTemplate,
+} from "../home/preview/previewTemplate/PreviewTemplate";
 
 function Portfolio() {
-    const {id}=useParams()
-    const {data,loading}=useQuery(`${PUBLIC_PORTFOLIO}/${id}`)
-    console.log(data,loading,'data')
+  const { id } = useParams();
+  const { data, loading } = useQuery(`${PUBLIC_PORTFOLIO}/${id}`);
+  console.log(data, loading, "data");
 
-    
-    if(loading||!data){
-        return <Loader2/>
-    }
-    const TemplateComponent = {
-        '1': MinimalTemplate,
-        '2': ModernTemplate,
-        '3': MinimalistTemplate,
-      }
-    const Component=TemplateComponent[data?.data?.data?.templateId??'1']
+  if (loading || !data) {
+    return <Loader2 />;
+  }
+  const TemplateComponent = {
+    1: MinimalTemplate,
+    2: ModernTemplate,
+    3: MinimalistTemplate,
+  };
+  const Component = TemplateComponent[data?.data?.data?.templateId ?? "1"];
   return (
-
     <div>
-        <Component data={data?.data?.data} template />
+      <Component
+        data={{ ...data?.data?.data, ...data?.data?.data?.user }}
+        template
+      />
     </div>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
